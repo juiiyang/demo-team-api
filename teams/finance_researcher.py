4533b8fad9881028341796b197b6242ff01cfad7
Ruiyang Guan <capgrey@linux.do>
2025-06-10 11:55:59 +0800

    update: 更换模型

--- teams/finance_researcher.py
+++ teams/finance_researcher.py
@@ -17,8 +17,8 @@ finance_agent = Agent(
     name="Finance Agent",
     role="Analyze financial data",
     agent_id="finance-agent",
-    model=OpenRouter(
-        id=team_settings.or_free_model,
+    model=OpenAIChat(
+        id=team_settings.gpt_41_mini,
         max_tokens=team_settings.default_max_completion_tokens,
         temperature=team_settings.default_temperature,
     ),
@@ -67,7 +67,7 @@ web_agent = Agent(
     name="Web Agent",
     role="Search the web for information",
     model=OpenAIChat(
-        id=team_settings.gpt_4_mini,
+        id=team_settings.gpt_41_mini,
         max_tokens=team_settings.default_max_completion_tokens,
         temperature=team_settings.default_temperature,
     ),
@@ -88,7 +88,7 @@ def get_finance_researcher_team(
     session_id: Optional[str] = None,
     debug_mode: bool = True,
 ):
-    model_id = model_id or team_settings.gpt_4
+    model_id = model_id or team_settings.gpt_41
 
     return Team(
         name="Finance Researcher Team",
--- teams/settings.py
+++ teams/settings.py
@@ -9,6 +9,8 @@ class TeamSettings(BaseSettings):
 
     gpt_4_mini: str = "gpt-4o-mini"
     gpt_4: str = "gpt-4o"
+    gpt_41_mini: str = "gpt-4.1-mini-2025-04-14"
+    gpt_41: str = "gpt-4.1-2025-04-14"
     or_free_model: str = "google/gemini-2.5-flash-preview-05-20"
     embedding_model: str = "text-embedding-3-small"
     default_max_completion_tokens: int = 16000
